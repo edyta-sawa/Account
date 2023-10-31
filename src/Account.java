eb993192c69de9f449c2613c7e9e05e9a42da1d0
sawae <$$Kacperus16>
2023-10-31 17:57:54 +0100

    bank account

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Account {
+import java.util.Scanner;
+
+class Account {
+    private double balance;
+
+    Account() {
+        this.balance = 0;
+    }
+
+    public void deposit(double newCash) {
+        this.balance += newCash;
+    }
+
+    public void withdraw(double minusCash) {
+        if (minusCash <= this.balance)
+            this.balance -= minusCash;
+        else
+            System.out.println("Forbidden.");
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Account account = new Account();
+        System.out.println("Your current balance is: " + account.getBalance());
+        System.out.println("Make a deposit: ");
+        Scanner input = new Scanner(System.in);
+        double newCash = input.nextDouble();
+        account.deposit(newCash);
+        System.out.println("Make a withdrawal: ");
+        double minusCash = input.nextDouble();
+        account.withdraw(minusCash);
+        System.out.println("Your current balance is: " + account.getBalance());
+    }
 }
